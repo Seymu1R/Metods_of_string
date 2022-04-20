7ff5733f5e5523407e68ba7166eb9c9889a68473
Seymu1R <102611811+Seymu1R@users.noreply.github.com>
2022-04-20 16:52:34 +0400

    first commit

---
+++ METHOD_FOR_STRINGS/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace METHOD_FOR_STRINGS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            MetHod_for_string("bU tasKI metHodla yAzmAliSiz");
+
+        }
+
+        public static void MetHod_for_string(string sentence ){
+            string result;
+           TextInfo new_word = CultureInfo.CurrentCulture.TextInfo;
+            result = sentence.ToLower();
+            result = new_word.ToTitleCase(sentence);
+            Console.WriteLine(result);
+
+            
+        }
+
+    }
+
+}
